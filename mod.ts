9fd9a6509265e005717bdd31108eb955e2a11708
Joel Huggett <joel.huggett@me.com>
2023-04-05 20:47:20 -0300

    chore: renamed exports to mod

--- mod.ts
+++ mod.ts
@@ -1,3 +1,6 @@
 export { DenoShell } from "./shells/deno-shell.ts";
 export { UnknownKeyCodeError, userInput } from "./user-input.ts";
+
+export type { Point } from "./point.ts";
+export type { XY } from "./xy.ts";
 export type { Key } from "./escape-codes/keys.ts";
