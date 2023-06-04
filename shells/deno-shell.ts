1f5a940d6f9855ad67e03b44a469ea0e8fa0d74a
Joel Huggett <joel.huggett@me.com>
2023-06-04 12:52:27 -0300

    feat: poll window size if SIGWINCH fails

--- shells/deno-shell.ts
+++ shells/deno-shell.ts
@@ -1,4 +1,8 @@
+import { XY } from "../mod.ts";
 import { Shell } from "./shell.ts";
+
+const sleep = (amount: number) =>
+  new Promise((resolve) => setTimeout(() => resolve(undefined), amount));
 
 export class DenoShell extends Shell {
   debugMode = false;
@@ -29,10 +33,46 @@ export class DenoShell extends Shell {
     return filledBuf;
   }
 
+  _previous_window_width?: number;
+  _previous_window_height?: number;
+  _should_poll_window_size = false;
+  private _check_window_size(onChange: () => void) {
+    const { columns: width, rows: height } = this.getShellSize();
+
+    if (
+      this._previous_window_height !== height ||
+      this._previous_window_width !== width
+    ) {
+      onChange();
+    }
+
+    this._previous_window_height = height;
+    this._previous_window_width = width;
+  }
+
+  private async _poll_window_size(onChange: () => void) {
+    this._should_poll_window_size = true;
+    this._previous_window_height = this.height;
+    this._previous_window_width = this.width;
+
+    while (this._should_poll_window_size) {
+      this._check_window_size(onChange);
+      await sleep(1000);
+    }
+  }
+
   onWindowResize(onEvent: () => void) {
-    Deno.addSignalListener("SIGWINCH", onEvent);
-    return {
-      stopListening: () => Deno.removeSignalListener("SIGWINCH", onEvent),
-    };
+    try {
+      Deno.addSignalListener("SIGWINCH", onEvent);
+      return {
+        stopListening: () => Deno.removeSignalListener("SIGWINCH", onEvent),
+      };
+    } catch {
+      // e.g. Windows doesn't support SIGWINCH
+      this._poll_window_size(onEvent);
+      return {
+        stopListening: () => (this._should_poll_window_size = false),
+      };
+    }
   }
 }
