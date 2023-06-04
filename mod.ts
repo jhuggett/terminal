07104996c33614ba976153624b39d8e515ad46ec
Joel Huggett <joel.huggett@me.com>
2023-06-03 22:05:24 -0300

    chore: updated exports

--- mod.ts
+++ mod.ts
@@ -2,6 +2,7 @@ export { DenoShell } from "./shells/deno-shell.ts";
 export { UnknownKeyCodeError, userInput } from "./user-input.ts";
 export { OutOfBoundsError, Layer } from "./layers/layer.ts";
 export { Box } from "./layers/box.ts";
+export { XYSet, combine } from "./xy.ts";
 
 export type { Point } from "./point.ts";
 export type { XY } from "./xy.ts";
